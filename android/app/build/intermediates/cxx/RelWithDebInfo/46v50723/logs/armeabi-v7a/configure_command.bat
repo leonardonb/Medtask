285f28153f2a307d5229005e0e6da007c9bdaf01
Leonardo Nunes <leonardonb@gmail.com>
2026-01-05 21:47:32 -0300

    retomando trabalho

---
+++ android/app/build/intermediates/cxx/RelWithDebInfo/46v50723/logs/armeabi-v7a/configure_command.bat
@@ -0,0 +1,20 @@
+@echo off
+"C:\\Users\\leona\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HD:\\flutter\\packages\\flutter_tools\\gradle\\src\\main\\scripts" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=29" ^
+  "-DANDROID_PLATFORM=android-29" ^
+  "-DANDROID_ABI=armeabi-v7a" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=armeabi-v7a" ^
+  "-DANDROID_NDK=C:\\Users\\leona\\AppData\\Local\\Android\\Sdk\\ndk\\27.0.12077973" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\leona\\AppData\\Local\\Android\\Sdk\\ndk\\27.0.12077973" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\leona\\AppData\\Local\\Android\\Sdk\\ndk\\27.0.12077973\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\leona\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=D:\\BCC\\PESSOAL\\Medtask\\android\\app\\build\\intermediates\\cxx\\RelWithDebInfo\\46v50723\\obj\\armeabi-v7a" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=D:\\BCC\\PESSOAL\\Medtask\\android\\app\\build\\intermediates\\cxx\\RelWithDebInfo\\46v50723\\obj\\armeabi-v7a" ^
+  "-DCMAKE_BUILD_TYPE=RelWithDebInfo" ^
+  "-BD:\\BCC\\PESSOAL\\Medtask\\android\\app\\.cxx\\RelWithDebInfo\\46v50723\\armeabi-v7a" ^
+  -GNinja ^
+  -Wno-dev ^
+  --no-warn-unused-cli
